b3add08a65a21d9231deae75da2fd1f01b6c793c
LenaAkimova <akimowa200053@gmail.com>
2021-06-29 13:55:28 +0300

    Внедрение зависимостей - промежуточный интерфейс

--- DI_IoC/Program.cs
+++ DI_IoC/Program.cs
@@ -16,27 +16,48 @@ namespace DI_IoC
         public string Lesson { get; set; }
     }
 
-/// <summary>
-/// Хотим создать расписание 
-/// </summary>
-    class SheduleManager
+    /// <summary>
+    /// Хотим создать расписание 
+    /// </summary>
+
+    interface ISHeduleManager
     {
+        Shedule GetShedule();
+    }
+
+    class SheduleManager:ISHeduleManager
+    {
+        /// <summary>
+        /// Создание расписания осоюого вида
+        /// </summary>
+        /// <returns></returns>
         public Shedule GetShedule()
         {
-            //
+            
             return new Shedule();
         }
     }
     /// <summary>
-    /// Для отображения расписания
+    /// Для отображения расписания 
+    /// Если поменяется код менеджера или появится много разных менеджеров 
+    /// чтобы не надо было менять вьюер тоже
     /// </summary>
     class SheduleViewer
     {
-        SheduleManager man = new SheduleManager();
+        private ISHeduleManager _man;
+
+        /// <summary>
+        /// Constructor injection
+        /// </summary>
+        /// <param name="man">Конкретная реализация менеджера</param>
+        public SheduleViewer(ISHeduleManager man)
+        {
+            _man = man;
+        }
 
         public void ViewShadule()
         {
-            man.GetShedule();
+            _man.GetShedule();
         }
     }
 
